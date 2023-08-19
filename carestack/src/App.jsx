5e0f48ea8d2b423e77f73577b4be1b48cccf4138
Hafis Muhammed <85503784+Haafii@users.noreply.github.com>
2023-08-20 03:21:36 +0530

    Add files via upload

--- carestack/src/App.jsx
+++ carestack/src/App.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 import User from "./pages/User";
+import AddEmployee from "./pages/AddEmployee";
+import AssignWork from "./pages/AssignWork";
+import AdminAnnouncement from "./pages/AdminAnnouncement";
+import JobCatalogue from "./pages/JobCatalogue";
+import JobLog from "./pages/JobLog";
+import ChatButton from "./pages/ChatButton";
+import Dashboard from "./pages/Dashboard.jsx";
+import Profile from "./pages/Profile";
+import Announcement from "./pages/Announcement";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="\" element={<Login />} />
-          <Route path="\admin" element={<Admin />} />
-          <Route path="\user" element={<User />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/addemployee" element={<AddEmployee />} />
+          <Route path="/assignwork" element={<AssignWork />} />
+          <Route
+            path="/admin/adminannouncement"
+            element={<AdminAnnouncement />}
+          />
+          <Route path="/admin/jobcata" element={<JobCatalogue />} />
+          <Route path="/admin/joblog" element={<JobLog />} />
+          <Route path="/chat" element={<ChatButton />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/announcement" element={<Announcement />} />
         </Routes>
       </Router>
     </div>
